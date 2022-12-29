d30184b49242936e036abbdd851e66b99786ce72
Syahirah Anuar <80741610+syahirahanuar03@users.noreply.github.com>
2022-12-29 11:43:27 +0800

    Final Update

--- G1_ASSIGNMENT_1.m
+++ G1_ASSIGNMENT_1.m
@@ -1,35 +1,35 @@
-function varargout = G1_ASSIGNMENT_1(varargin)
-% G1_ASSIGNMENT_1 MATLAB code for G1_ASSIGNMENT_1.fig
-%      G1_ASSIGNMENT_1, by itself, creates a new G1_ASSIGNMENT_1 or raises the existing
+function varargout = G1_PUMA560(varargin)
+% G1_PUMA560 MATLAB code for G1_PUMA560.fig
+%      G1_PUMA560, by itself, creates a new G1_PUMA560 or raises the existing
 %      singleton*.
 %
-%      H = G1_ASSIGNMENT_1 returns the handle to a new G1_ASSIGNMENT_1 or the handle to
+%      H = G1_PUMA560 returns the handle to a new G1_PUMA560 or the handle to
 %      the existing singleton*.
 %
-%      G1_ASSIGNMENT_1('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in G1_ASSIGNMENT_1.M with the given input arguments.
+%      G1_PUMA560('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in G1_PUMA560.M with the given input arguments.
 %
-%      G1_ASSIGNMENT_1('Property','Value',...) creates a new G1_ASSIGNMENT_1 or raises the
+%      G1_PUMA560('Property','Value',...) creates a new G1_PUMA560 or raises the
 %      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before G1_ASSIGNMENT_1_OpeningFcn gets called.  An
+%      applied to the GUI before G1_PUMA560_OpeningFcn gets called.  An
 %      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to G1_ASSIGNMENT_1_OpeningFcn via varargin.
+%      stop.  All inputs are passed to G1_PUMA560_OpeningFcn via varargin.
 %
 %      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
 %      instance to run (singleton)".
 %
 % See also: GUIDE, GUIDATA, GUIHANDLES
 
-% Edit the above text to modify the response to help G1_ASSIGNMENT_1
+% Edit the above text to modify the response to help G1_PUMA560
 
-% Last Modified by GUIDE v2.5 29-Dec-2022 08:55:21
+% Last Modified by GUIDE v2.5 29-Dec-2022 11:37:19
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
-                   'gui_OpeningFcn', @G1_ASSIGNMENT_1_OpeningFcn, ...
-                   'gui_OutputFcn',  @G1_ASSIGNMENT_1_OutputFcn, ...
+                   'gui_OpeningFcn', @G1_PUMA560_OpeningFcn, ...
+                   'gui_OutputFcn',  @G1_PUMA560_OutputFcn, ...
                    'gui_LayoutFcn',  [] , ...
                    'gui_Callback',   []);
 if nargin && ischar(varargin{1})
@@ -44,26 +44,26 @@ end
 % End initialization code - DO NOT EDIT
 
 
-% --- Executes just before G1_ASSIGNMENT_1 is made visible.
-function G1_ASSIGNMENT_1_OpeningFcn(hObject, eventdata, handles, varargin)
+% --- Executes just before G1_PUMA560 is made visible.
+function G1_PUMA560_OpeningFcn(hObject, eventdata, handles, varargin)
 % This function has no output args, see OutputFcn.
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to G1_ASSIGNMENT_1 (see VARARGIN)
+% varargin   command line arguments to G1_PUMA560 (see VARARGIN)
 
-% Choose default command line output for G1_ASSIGNMENT_1
+% Choose default command line output for G1_PUMA560
 handles.output = hObject;
 
 % Update handles structure
 guidata(hObject, handles);
 
-% UIWAIT makes G1_ASSIGNMENT_1 wait for user response (see UIRESUME)
+% UIWAIT makes G1_PUMA560 wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
 
 
 % --- Outputs from this function are returned to the command line.
-function varargout = G1_ASSIGNMENT_1_OutputFcn(hObject, eventdata, handles) 
+function varargout = G1_PUMA560_OutputFcn(hObject, eventdata, handles) 
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -263,35 +263,6 @@ function fwdbutton_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-
-L1 = 0; L2 = 431.8; L3 = -20.32; L4 = 0; L5 = 0; L6 = 0;
-
-L(1) = Link ([0 0 L1 0 pi/2]);
-L(2) = Link ([0 0 L2 0 pi/2]);
-L(3) = Link ([0 0 L3 0 pi/2]);
-L(4) = Link ([0 0 L4 0 pi/2]);
-L(5) = Link ([0 0 L5 0 pi/2]);
-L(6) = Link ([0 0 L6 0 pi/2]);
-
-th1 = str2double (handles.edit1.String)*pi/180;
-th2 = str2double (handles.edit2.String)*pi/180;
-th3 = str2double (handles.edit3.String)*pi/180;
-th4 = str2double (handles.edit4.String)*pi/180;
-th5 = str2double (handles.edit5.String)*pi/180;
-th6 = str2double (handles.edit6.String)*pi/180;
-
-Robot = SerialLink(L);
-Robot.name = 'G1_ASSIGNMENT_1';
-Robot.plot ([th1 th2 th3 th4 th5 th6]);
-T = Robot.fkine ([th1 th2 th3 th4 th5 th6]);
-
-handles.edit7.String = num2str(floor(T.t(1)));  %Pos X
-handles.edit8.String = num2str(floor(T.t(2)));  %Pos Y
-handles.edit9.String = num2str(floor(T.t(3)));  %Pos Z
-
-%---------------------------------------------------------------%
-%---------------CALCULATION HTM---------------------------%
-
 %L1
 A1 = 0; Alpha1 = -pi/2; D1 = 0; T1 = 0;
 
@@ -310,42 +281,67 @@ A5 = 0; Alpha5 = pi/2; D5=0; T5 = -pi/4;
 %L6
 A6 = 0; Alpha6 = 0; D6 = 56.25; T6 = pi/2;
 
-H1 = [cos(T1) -sin(T1)*cos(Alpha1) sin(T1)*sin(Alpha1) A1*cos(T1);
-        sin(T1) cos(T1)*cos(Alpha1) -cos(T1)*sin(Alpha1) A1*sin(T1);
-        0       sin(Alpha1)         cos(Alpha1)          D1;
-        0           0                   0                1;];
+%L = LInk ([ th d a alpha])
 
-H2 = [cos(T2) -sin(T2)*cos(Alpha2) sin(T2)*sin(Alpha2) A2*cos(T2);
-        sin(T2) cos(T2)*cos(Alpha2) -cos(T2)*sin(Alpha2) A2*sin(T2);
-        0       sin(Alpha2)         cos(Alpha2)          D2;
-        0           0                   0                1;];
+L(1) = Link ([T1 D1 A1 Alpha1]);
+L(2) = Link ([T2 D2 A2 Alpha2]);
+L(3) = Link ([T3 D3 A3 Alpha3]);
+L(4) = Link ([T4 D4 A4 Alpha4]);
+L(5) = Link ([T5 D5 A5 Alpha5]);
+L(6) = Link ([T6 D6 A6 Alpha6]);
 
-H3 = [cos(T3) -sin(T3)*cos(Alpha3) sin(T3)*sin(Alpha3) A3*cos(T3);
-        sin(T3) cos(T3)*cos(Alpha3) -cos(T3)*sin(Alpha3) A3*sin(T3);
-        0       sin(Alpha3)         cos(Alpha1)          D3;
-        0           0                   0                1;];
+th1 = str2double (handles.edit1.String)*pi/180;
+th2 = str2double (handles.edit2.String)*pi/180;
+th3 = str2double (handles.edit3.String)*pi/180;
+th4 = str2double (handles.edit4.String)*pi/180;
+th5 = str2double (handles.edit5.String)*pi/180;
+th6 = str2double (handles.edit6.String)*pi/180;
 
-H4 = [cos(T4) -sin(T4)*cos(Alpha4) sin(T4)*sin(Alpha4) A4*cos(T4);
-        sin(T4) cos(T4)*cos(Alpha4) -cos(T4)*sin(Alpha4) A4*sin(T4);
-        0       sin(Alpha4)         cos(Alpha4)          D4;
-        0           0                   0                1;];
- 
-H5 = [cos(T5) -sin(T5)*cos(Alpha5) sin(T1)*sin(Alpha5) A1*cos(T5);
-        sin(T5) cos(T5)*cos(Alpha5) -cos(T1)*sin(Alpha5) A1*sin(T5);
-        0       sin(Alpha5)         cos(Alpha5)          D5;
-        0           0                   0                1;];
+Robot = SerialLink(L);
+Robot.name = 'G1_PUMA_560';
+Robot.plot ([th1 th2 th3 th4 th5 th6]);
+T = Robot.fkine ([th1 th2 th3 th4 th5 th6]);
+
+handles.edit7.String = num2str(floor(T.t(1)));  %Pos X
+handles.edit8.String = num2str(floor(T.t(2)));  %Pos Y
+handles.edit9.String = num2str(floor(T.t(3)));  %Pos Z
+
+%---------------------------------------------------------------%
+%---------------CALCULATION HTM---------------------------%
+
+H1 =   [cos(T1) -sin(T1)*cos(Alpha1) sin(T1)*sin(Alpha1)  A1*cos(T1);
+        sin(T1) cos(T1)*cos(Alpha1)  -cos(T1)*sin(Alpha1) A1*sin(T1);
+        0       sin(Alpha1)          cos(Alpha1)          D1;
+        0       0                    0                    1;];
+
+H2 = [cos(T2) -sin(T2)*cos(Alpha2) sin(T2)*sin(Alpha2)  A2*cos(T2);
+      sin(T2) cos(T2)*cos(Alpha2)  -cos(T2)*sin(Alpha2) A2*sin(T2);
+      0       sin(Alpha2)          cos(Alpha2)          D2;
+      0       0                    0                    1;];
+
+H3 = [cos(T3) -sin(T3)*cos(Alpha3) sin(T3)*sin(Alpha3)  A3*cos(T3);
+      sin(T3) cos(T3)*cos(Alpha3)  -cos(T3)*sin(Alpha3) A3*sin(T3);
+      0       sin(Alpha3)          cos(Alpha1)          D3;
+      0       0                    0                    1;];
+
+H4 = [cos(T4) -sin(T4)*cos(Alpha4) sin(T4)*sin(Alpha4)  A4*cos(T4);
+      sin(T4) cos(T4)*cos(Alpha4)  -cos(T4)*sin(Alpha4) A4*sin(T4);
+      0       sin(Alpha4)          cos(Alpha4)          D4;
+      0       0                    0                    1;];
     
-H6 = [cos(T6) -sin(T6)*cos(Alpha6) sin(T6)*sin(Alpha6) A1*cos(T6);
-        sin(T6) cos(T6)*cos(Alpha6) -cos(T6)*sin(Alpha6) A1*sin(T6);
-        0       sin(Alpha6)         cos(Alpha6)          D6;
-        0           0                   0                1;];
+H5 = [cos(T5) -sin(T5)*cos(Alpha5) sin(T1)*sin(Alpha5)  A1*cos(T5);
+      sin(T5) cos(T5)*cos(Alpha5)  -cos(T1)*sin(Alpha5) A1*sin(T5);
+      0       sin(Alpha5)          cos(Alpha5)          D5;
+      0       0                    0                    1;];
+    
+H6 = [cos(T6) -sin(T6)*cos(Alpha6) sin(T6)*sin(Alpha6)  A1*cos(T6);
+      sin(T6) cos(T6)*cos(Alpha6)  -cos(T6)*sin(Alpha6) A1*sin(T6);
+      0       sin(Alpha6)          cos(Alpha6)          D6;
+      0       0                    0                    1;];
 
 H0_6 = H1 * H2 * H3 * H4 * H5 * H6;
 
-
-
-handles.text13.String = num2str(H0_6);
-
+handles.text11.String = num2str(round(H0_6,3));
 
 % --- Executes on button press in invbutton.
 function invbutton_Callback(hObject, eventdata, handles)
@@ -368,7 +364,7 @@ L(6) = Link ([0 0 L6 0 pi/2]);
 
 
 Robot = SerialLink(L);
-Robot.name = 'G1_ASSIGNMENT_1';
+Robot.name = 'G1_PUMA_560';
 
 T = [1   0   0   PX ;
      0   1   1   PY ;
@@ -385,12 +381,10 @@ handles.edit5.String = num2str(floor(J(5)));
 handles.edit6.String = num2str(floor(J(6)));
 
 Robot.plot(J*pi/180); %done
- 
- 
 
 
 % --- Executes during object creation, after setting all properties.
-function text13_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to text13 (see GCBO)
+function text11_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to text11 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    empty - handles not created until after all CreateFcns called
